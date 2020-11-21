d2ffcdd06c2fde5736880191baa7ec98e50ce701
Ковган Татьяна <kovgantatyana@mail.ru>
2020-11-21 16:47:59 +0300

    init commit

---
+++ src/main/java/newwiki/config/NewwikiApplication.java
@@ -0,0 +1,13 @@
+package newwiki.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/newwiki/NewwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package newwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
